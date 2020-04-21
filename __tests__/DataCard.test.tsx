93e532eb8f2087c29662a30825f970e4a659a6db
Ondřej Tománek <andrewtomanek@gmail.com>
2020-04-21 10:23:21 +0200

    Create DataCard.test.tsx

---
+++ __tests__/DataCard.test.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { shallow } from "enzyme";
+import DataCard from "../src/components/DataCard";
+
+const props = {
+  item: {
+    bílkoviny: 1,
+    cena: 6,
+    id: 1,
+    image: "rajčata",
+    kalorie: 19,
+    množství: 1,
+    picked: false,
+    sacharidy: 2.9,
+    tuky: 0.2,
+    vláknina: 1,
+  },
+};
+
+describe("Renders DataCard", () => {
+  it("renders DataCard component", () => {
+    const wrapper = shallow(<DataCard {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("renders text", () => {
+    const wrapper = shallow(<DataCard {...props} />);
+    expect(wrapper.children()).toHaveLength(8);
+  });
+});

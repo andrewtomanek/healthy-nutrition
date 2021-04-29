032458fe11f4908e98842cab2b0ec71a10d2b4f3
Ondřej Tománek <andrewtomanek@gmail.com>
2021-04-29 17:57:38 +0200

    Create store file

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,10 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import createSagaMiddleware from "redux-saga";
 import { Provider } from "react-redux";
-import rootReducer from "./store/reducers/rootReducer";
-import { watchStoreBuilder } from "./store/sagas/mainSaga";
+import store from "./store/store";
 
 import App from "./App";
 import Loader from "./components/Loader";
-
-const composeEnhancers =
-  (window["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] as typeof compose) || compose;
-const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
-);
-
-sagaMiddleware.run(watchStoreBuilder);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
 import { compose } from "redux";
 import createSagaMiddleware from "redux-saga";
-import  rootReducer  from "./reducers/rootReducer";
+import rootReducer from "./reducers/rootReducer";
 import { watchStoreBuilder } from "./sagas/mainSaga";
 
-const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose;
+const composeEnhancers =
+  (window["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] as typeof compose) || compose;
 const sagaMiddleware = createSagaMiddleware();
 
-export default function configureStore() {
-  const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(sagaMiddleware))
-  );
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
 
-  sagaMiddleware.run(watchStoreBuilder);
+sagaMiddleware.run(watchStoreBuilder);
 
-  return store;
-}
+export default store;

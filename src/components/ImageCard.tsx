c0a71f81cff533d23d19187116f0e37d5213a4db
Ondřej Tománek <andrewtomanek@gmail.com>
2021-04-30 23:41:53 +0200

    Fetch images from cloud

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
+import { storage } from "../firebase/firebase";
 import { FoodUnit } from "../types/shared";
-
-function importAll(r: any) {
-  let images = {};
-  r.keys().map((item: string) => {
-    return (images[item.replace("./", "")] = r(item));
-  });
-  return images;
-}
-
-const images = importAll(
-  require.context("../../public/img/cont", false, /\.(png|jpe?g|svg)$/)
-);
 
 type Props = {
   item: FoodUnit;
 };
 
 const ImageCard = ({ item }: Props) => {
+  const [imageUrl, setImageUrl] = useState<string>("");
+
+  const getImageUrl = async () => {
+    return await storage
+      .ref("images")
+      .child(item.image + ".jpg")
+      .getDownloadURL();
+  };
+
+  useEffect(() => {
+    getImageUrl().then((url) => setImageUrl(url));
+  }, []);
+
   return (
     <PictureContainer style={{ zIndex: item.picked ? 1 : 3 }}>
-      <PictureCard
-        src={images[item.image + ".jpg"].default}
-        alt={item.image}
-        style={{
-          filter: item.picked ? `hue-rotate(180deg)` : `hue-rotate(0deg)`,
-        }}
-      />
+      {imageUrl ? (
+        <PictureCard
+          src={imageUrl}
+          alt={item.image}
+          style={{
+            filter: item.picked ? `hue-rotate(180deg)` : `hue-rotate(0deg)`,
+          }}
+        />
+      ) : (
+        <svg width="1024" height="768" viewBox="0 0 100 100">
+          <rect width="1024" height="768" rx="10" ry="10" fill="#ff6600" />
+        </svg>
+      )}
     </PictureContainer>
   );
 };
